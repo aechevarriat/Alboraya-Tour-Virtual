866923699fe4509b75ad807ef78d10d901008c77
aechevarriat <aliciaechevarria27@gmail.com>
2022-08-15 22:15:53 +0200

    subido de nuevo

--- data.js
+++ data.js
@@ -24,14 +24,14 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": -0.2725885431351962,
-        "pitch": 0.10738017226985441,
+        "yaw": -0.011423973285783262,
+        "pitch": 0.007811913519740088,
         "fov": 1.41569238083293
       },
       "linkHotspots": [
         {
-          "yaw": -0.09811456592615464,
-          "pitch": 0.2721285049476254,
+          "yaw": -0.0973710975387796,
+          "pitch": 0.3215044392791988,
           "rotation": 0,
           "target": "1-salon"
         }
@@ -68,122 +68,28 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 3.0103156337083687,
-          "pitch": 0.16873644268876475,
-          "rotation": 0,
-          "target": "2-cocina"
-        },
-        {
-          "yaw": -2.7002492735810577,
-          "pitch": 0.14633853923125528,
+          "yaw": -2.6923837723353543,
+          "pitch": 0.16573758596591048,
           "rotation": 0,
           "target": "0-entrada"
         },
         {
-          "yaw": 2.014845367816873,
-          "pitch": 0.18914856988285678,
+          "yaw": 3.0109431278577734,
+          "pitch": 0.19005022819079898,
           "rotation": 0,
-          "target": "4-cocina"
+          "target": "3-cocina"
+        },
+        {
+          "yaw": 2.0316387865536285,
+          "pitch": 0.16309108992310328,
+          "rotation": 0,
+          "target": "2-cocina"
         }
       ],
       "infoHotspots": []
     },
     {
       "id": "2-cocina",
-      "name": "COCINA",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "yaw": -0.4150733372567572,
-        "pitch": 0.14972834246167643,
-        "fov": 1.41569238083293
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.3886017153896262,
-          "pitch": 0.1365097219326934,
-          "rotation": 0,
-          "target": "3-galeria"
-        },
-        {
-          "yaw": -1.2587129273016764,
-          "pitch": 0.18757849853417774,
-          "rotation": 0,
-          "target": "4-cocina"
-        },
-        {
-          "yaw": -2.2409191369393664,
-          "pitch": 0.2261254991984245,
-          "rotation": 0,
-          "target": "1-salon"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-galeria",
-      "name": "GALERIA",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "yaw": 0.03617595691951081,
-        "pitch": 0.050777437878313236,
-        "fov": 1.41569238083293
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.7138985028815625,
-          "pitch": 0.43443662444263076,
-          "rotation": 5.497787143782138,
-          "target": "2-cocina"
-        },
-        {
-          "yaw": -1.428826506410811,
-          "pitch": 0.3008816990015344,
-          "rotation": 0,
-          "target": "4-cocina"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-cocina",
       "name": "COCINA.",
       "levels": [
         {
@@ -206,34 +112,128 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": 0.17135959928671696,
-        "pitch": 0.02602331063132368,
-        "fov": 1.41569238083293
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0.0752732187209979,
-          "pitch": 0.14446564712071108,
+          "yaw": 0.06957394488416924,
+          "pitch": 0.15756365881417445,
           "rotation": 0,
-          "target": "3-galeria"
+          "target": "4-galeria"
         },
         {
-          "yaw": 1.9254906078540852,
-          "pitch": 0.2533629268966262,
+          "yaw": 0.9006881326720197,
+          "pitch": 0.17366866514266555,
+          "rotation": 0,
+          "target": "3-cocina"
+        },
+        {
+          "yaw": 1.90015071883718,
+          "pitch": 0.23644670793956024,
           "rotation": 0,
           "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cocina",
+      "name": "COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": 0.8448372367940937,
-          "pitch": 0.1988004342726697,
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3613127206750555,
+          "pitch": 0.13324207085126005,
+          "rotation": 0,
+          "target": "4-galeria"
+        },
+        {
+          "yaw": -1.2570171728547095,
+          "pitch": 0.13521985893185473,
           "rotation": 0,
           "target": "2-cocina"
+        },
+        {
+          "yaw": -2.2473628030445063,
+          "pitch": 0.3278068048130134,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-galeria",
+      "name": "GALERIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4131647887121854,
+          "pitch": 0.22082141254834298,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": -1.7010811772700656,
+          "pitch": 0.38734420676935954,
+          "rotation": 5.497787143782138,
+          "target": "3-cocina"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Tour Alboraya  360",
+  "name": "ALBORAYA TOUR VIRTUAL",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
